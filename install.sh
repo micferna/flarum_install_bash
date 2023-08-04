ea226c34542f299d4c7b50a1a037f4cc386cfff4
micferna <122208805+micferna@users.noreply.github.com>
2023-08-05 00:10:33 +0200

    menu
    Ajout d'un menu

--- install.sh
+++ install.sh
@@ -6,12 +6,34 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-# Demande les informations sur le domaine et la base de données
-read -p "Entrez le nom de domaine (par exemple, example.com) : " domain
-read -p "Entrez le nom de la base de données : " dbname
-read -p "Entrez le nom d'utilisateur de la base de données : " dbuser
-read -s -p "Entrez le mot de passe de la base de données : " dbpass
+# Fonction pour afficher le texte en couleur
+color_text() {
+    local color="$1"
+    local text="$2"
+    echo -e "\e[${color}m${text}\e[0m"
+}
+
+# Affichage du menu
+color_text "32;1" "=== Menu d'information ==="
+read -p "$(color_text "36" 'Entrez le nom de domaine (par exemple, example.com) : ')" domain
+read -p "$(color_text "36" 'Entrez le nom de la base de données : ')" dbname
+read -p "$(color_text "36" 'Entrez le nom d\'utilisateur de la base de données : ')" dbuser
+read -s -p "$(color_text "36" 'Entrez le mot de passe de la base de données : ')" dbpass
 echo
+
+# Calcul de la largeur des colonnes
+col_width=0
+for ((i = 0; i < ${#table[@]}; i += 2)); do
+    len=${#table[i]}
+    if ((len > col_width)); then
+        col_width=$len
+    fi
+done
+
+# Affichage du tableau
+for ((i = 0; i < ${#table[@]}; i += 2)); do
+    printf "%-${col_width}s %s\n" "${table[i]}" "${table[i + 1]}"
+done
 
 # Fonction pour exécuter mysql_secure_installation
 secure_mysql() {

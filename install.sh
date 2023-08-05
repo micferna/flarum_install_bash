58237e730a530a054e266fdec4fab6dd2ee2502d
micferna <admin@micferna.eu>
2023-08-05 15:45:41 +0200

    ssl

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Couleurs
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
 # Vérifie si le script est exécuté en tant que root
 if [[ $EUID -ne 0 ]]; then
    echo "Ce script doit être exécuté en tant que root." 
@@ -8,11 +15,46 @@ fi
 
 # Demande les informations sur le domaine et la base de données
 clear
-read -p "Entrez le nom de domaine (par exemple, example.com) : " domain
+echo -e "${GREEN}-------------------------------------------------"
+echo -e "      Bienvenue dans l'installateur de Flarum    "
+echo -e "-------------------------------------------------${NC}\n"
+
+# Prompt pour le domaine
+echo -e "${YELLOW}Nom de domaine:${NC}"
+read -p "(par exemple, example.com) : " domain
+
+# Vérifie que le nom de domaine est valide
+if ! echo "$domain" | grep -P "^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\\.)+[A-Za-z]{2,6}$" > /dev/null 2>&1; then
+  echo -e "${RED}Le nom de domaine n'est pas valide.${NC}"
+  exit 1
+fi
+
+# Demande l'adresse e-mail pour Certbot
+echo -e "\n${YELLOW}Adresse e-mail pour les notifications SSL:${NC}"
+read -p "Entrez votre adresse e-mail : " email
+
+# Vérifie que l'adresse e-mail est valide
+if ! echo "$email" | grep -P "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$" > /dev/null 2>&1; then
+  echo -e "${RED}L'adresse e-mail n'est pas valide.${NC}"
+  exit 1
+fi
+
+# Prompt pour le nom de la base de données
+echo -e "\n${YELLOW}Base de données:${NC}"
 read -p "Entrez le nom de la base de données : " dbname
+
+# Prompt pour le nom d'utilisateur de la base de données
+echo -e "\n${YELLOW}Utilisateur de la base de données:${NC}"
 read -p "Entrez le nom d'utilisateur de la base de données : " dbuser
+
+# Prompt pour le mot de passe de la base de données
+echo -e "\n${YELLOW}Mot de passe de la base de données:${NC}"
 read -s -p "Entrez le mot de passe de la base de données : " dbpass
 echo
+
+generate_ssl() {
+    certbot --nginx --non-interactive --agree-tos --email $email -d $domain
+}
 
 # Fonction pour exécuter mysql_secure_installation
 secure_mysql() {
@@ -61,13 +103,15 @@ install_composer() {
 }
 
 # Configure le fuseau horaire
-sudo dpkg-reconfigure tzdata
+dpkg-reconfigure tzdata
 
 # Met à jour le système
 apt update && apt upgrade -y
 
 # Installe les paquets requis
-apt install -y zip unzip curl wget git php php-cli php-fpm php-common php-mbstring php-gd php-xml php-mysql php-curl php-zip mariadb-server nginx expect
+apt install -y zip unzip curl wget git php php-cli php-fpm php-json php-common php-mbstring php-gd php-xml php-mysql php-curl php-zip mariadb-server nginx expect certbot python3-certbot-nginx
+rm -rf /var/www/html
+apt autoremove -y --purge apache2 
 
 # Active les modules PHP nécessaires
 phpenmod dom gd json mbstring openssl pdo_mysql tokenizer
@@ -76,19 +120,19 @@ phpenmod dom gd json mbstring openssl pdo_mysql tokenizer
 secure_mysql
 
 # Crée la base de données et l'utilisateur pour Flarum
-sudo mysql -u root -e "CREATE DATABASE $dbname;"
-sudo mysql -u root -e "GRANT ALL ON $dbname.* TO '$dbuser' IDENTIFIED BY '$dbpass';"
-sudo mysql -u root -e "FLUSH PRIVILEGES;"
+mysql -u root -e "CREATE DATABASE $dbname;"
+mysql -u root -e "GRANT ALL ON $dbname.* TO '$dbuser' IDENTIFIED BY '$dbpass';"
+mysql -u root -e "FLUSH PRIVILEGES;"
 
 # Télécharge et installe Composer
 install_composer
 
 # Crée le répertoire pour Flarum
-sudo mkdir -p /var/www/flarum
-sudo chown -R www-data:www-data /var/www/flarum
+mkdir -p /var/www/flarum
+chown -R www-data:www-data /var/www/flarum
 
 # Configure Nginx pour Flarum
-cat <<EOF | sudo tee /etc/nginx/sites-available/flarum.conf
+cat <<EOF | tee /etc/nginx/sites-available/flarum.conf
 server {
     listen [::]:80;
     listen 80;
@@ -110,15 +154,18 @@ server {
 }
 EOF
 
-sudo ln -s /etc/nginx/sites-available/flarum.conf /etc/nginx/sites-enabled/
+ln -s /etc/nginx/sites-available/flarum.conf /etc/nginx/sites-enabled/
 rm -rf /etc/nginx/sites-available/default
 rm -rf /etc/nginx/sites-enabled/default
 
+# Génération du ssl
+generate_ssl
+
 # Teste la configuration de Nginx
-sudo nginx -t
+nginx -t
 
 # Redémarre Nginx
-sudo systemctl restart nginx
+systemctl restart nginx
 
 # Télécharge et installe Flarum
 cd /var/www/flarum
@@ -129,4 +176,4 @@ sudo -u www-data composer require bilgehanars/packman:"*"
 sudo -u www-data composer require flarum/package-manager:"@beta"
 
 
-echo "Flarum a été installé avec succès sur $domain."
+echo -e "Flarum a été installé avec ${GREEN}succès${NC} sur ${domain}."
